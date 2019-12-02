4b15897515ab005feabf4c84bc6fc8fa8e2e051b
limeiman <limeiman@weface.com>
2019-12-02 14:36:23 +0800

    每天都要开心鸭鸭！

--- src/main/java/com/springbootgit/MyUtils/PhotoHandle.java
+++ src/main/java/com/springbootgit/MyUtils/PhotoHandle.java
@@ -1,4 +1,79 @@
 package com.springbootgit.MyUtils;
 
+import sun.misc.BASE64Encoder;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class PhotoHandle {
+
+    public static void download(String urlString,int i){
+
+        try {
+            // 构造URL
+            URL url = new URL(urlString);
+            // 打开连接
+            URLConnection con = url.openConnection();
+            // 输入流
+            InputStream is = con.getInputStream();
+            // 1K的数据缓冲
+            byte[] bs = new byte[1024];
+            // 读取到的数据长度
+            int len;
+            // 输出的文件流
+            String filename = "D:\\downPic/" + i + ".jpg";  //下载路径及下载图片名称
+            File file = new File(filename);
+            FileOutputStream os = new FileOutputStream(file, true);
+            // 开始读取
+            while ((len = is.read(bs)) != -1) {
+                os.write(bs, 0, len);
+            }
+            System.out.println(i);
+            // 完毕，关闭所有链接
+            os.close();
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //链接url下载图片
+    private static void downloadPicture(String urlList,String path) {
+        URL url = null;
+        try {
+            url = new URL(urlList);
+            DataInputStream dataInputStream = new DataInputStream(url.openStream());
+
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = dataInputStream.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+            BASE64Encoder encoder = new BASE64Encoder();
+            String encode = encoder.encode(buffer);//返回Base64编码过的字节数组字符串
+            System.out.println(encode);
+            fileOutputStream.write(output.toByteArray());
+            dataInputStream.close();
+            fileOutputStream.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        String url="http://socialsecurity.oss-cn-beijing.aliyuncs.com/civil/7b227eb5c221c9281a8d0005aeced851/1674d0a8-b2cc-4ee4-9285-dab3ba56ada6.jpg";
+
+        PhotoHandle.download(url,5  );
+    }
 }
